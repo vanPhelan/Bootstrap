1a596fedd2a2277a72301ad27842ef890843c44a
Van Phelan <vephelan@gmail.com>
2021-09-28 12:22:14 -0500

    Added OpenGL

--- GraphicsProject/main.cpp
+++ GraphicsProject/main.cpp
@@ -1,12 +1,33 @@
 #include "gl_core_4_4.h"
 #include "GLFW/glfw3.h"
+#include <iostream>
 
 int main() {
-	if (!glfwInit()) {
+	//Initialize GLFW
+	if (glfwInit() == GLFW_FALSE) {
 		return -1;
 	}
 
-	glfwTerminate();
+	//Create a window
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Test Window", nullptr, nullptr);
+	if (!window) {
+		glfwTerminate();
+		return -2;
+	}
+	glfwMakeContextCurrent(window);
 
+	//Load OpenGL
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+	int majorVersion = ogl_GetMajorVersion();
+	int minorVersion = ogl_GetMinorVersion();
+	printf("OpenGL version %i.%i\n", majorVersion, minorVersion);
+
+	//Cleanup and exit
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
