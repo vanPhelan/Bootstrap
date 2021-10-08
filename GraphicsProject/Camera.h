3d968f8aa1d08f3f3129939b7893ac3d4232a382
Van Phelan <vephelan@gmail.com>
2021-10-08 15:33:10 -0500

    Changed the camera to support movement

--- GraphicsProject/Camera.h
+++ GraphicsProject/Camera.h
@@ -9,8 +9,15 @@ public:
 		: m_fieldOfView{ fieldOfView }, m_nearClip{ nearClip }, m_farClip{ farClip } {}
 	~Camera() {}
 
-	glm::mat4 getTransform() { return m_transform; }
-	void setTransform(glm::mat4 transform) { m_transform = transform; }
+	//glm::mat4 getTransform() { return m_transform; }
+	//void setTransform(glm::mat4 transform) { m_transform = transform; }
+	glm::mat4 getViewMatrix();
+	glm::vec3 getPosition() { return m_position; }
+	void setPosition(glm::vec3 position) { m_position = position; }
+	float getYaw() { return m_theta; }
+	void setYaw(float theta) { m_theta = theta; }
+	float getPitch() { return m_phi; }
+	void setPitch(float phi) { m_phi = phi; }
 	float getFieldOfView() { return m_fieldOfView; }
 	void setFieldOfView(float fieldOfView) { m_fieldOfView = fieldOfView; }
 	float getNearClip() { return m_nearClip; }
@@ -19,9 +26,13 @@ public:
 	void setFarClip(float farClip) { m_farClip = farClip; }
 
 private:
-	glm::mat4 m_transform = glm::mat4(1.0f);
-	float m_fieldOfView = 45.0f;
-	float m_nearClip = 0.001f;
-	float m_farClip = 1000.0f;
+	//glm::mat4 m_transform = glm::mat4(1.0f);
+	glm::vec3 m_position{ 0.0f, 0.0f, 0.0f };
+	float m_theta{ 0.0f };
+	float m_phi{ 0.0f };
+
+	float m_fieldOfView{ 45.0f };
+	float m_nearClip{ 0.001f };
+	float m_farClip{ 1000.0f };
 };
 
